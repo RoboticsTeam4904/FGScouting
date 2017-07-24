ef9e1850441bd6f9115721b119f27748a33b5d87
Billy <wwpierce020602@gmail.com>
2017-07-23 18:58:59 -0700

    formatted and changed database name

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', {
-    title: 'Team 4904s Stand Scouting'
-  });
+    res.render('index', {
+        title: 'Team 4904s Stand Scouting'
+    });
 });
 router.post('/pushData', function(req, res, next) {
-  var db = req.db;
-  var usersHolder = db.get('users');
-  usersHolder.insert(req.body, function (err, result) {
-      if (err)
-         res.send('Error');
-      else
-        res.send('Success');
-  });
+    var db = req.db;
+    var resultsHolder = db.get('formResults');
+    resultsHolder.insert(req.body, function(err, result) {
+        if (err)
+            res.send('Error');
+        else
+            res.send('Success');
+    });
 });
 
 
