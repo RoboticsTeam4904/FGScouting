e1494ba23568479e3f40e0d9a206dad529d2070a
billwpierce <wwpierce020602@gmail.com>
2017-08-07 18:23:25 -0700

    Refactored to img

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req,res,next){
