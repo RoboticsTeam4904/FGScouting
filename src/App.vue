a521d025a4c986db50d1a56718c876394bb29c12
Izzy Swart <syntacticsugarglider@gmail.com>
2017-10-03 22:10:19 -0700

    fixed accesibility attributes, lots of unfinished auth work, login page, online/offline autoswitching callbacks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <Loader v-if="applicationStatus==='initializing'"></Loader>
+  <span>
+    <Loader v-if="applicationStatus==='initializing'"></Loader>
+    <ErrorPage v-if="applicationStatus==='fatalError'" :text="failureText"></ErrorPage>
+    <SignInPage v-if="applicationStatus==='signIn'" :errorMessage="signInErrorMessage" :callback="signIn"></SignInPage>
+    <ScoutingForm v-if="applicationStatus==='scouting'"></ScoutingForm>
+  </span>
 </template>
 
 <script>
 import Loader from './components/Loader'
+import ErrorPage from './components/ErrorPage'
+import SignInPage from './components/SignInPage'
+import ScoutingForm from './components/ScoutingForm'
 
 export default {
   name: 'app',
-  components: {Loader},
+  components: {Loader, ErrorPage, SignInPage, ScoutingForm},
   data() {
     return {
-      applicationStatus: 'initializing'
+      applicationStatus: 'initializing',
+      isConnected: false,
+      failureText: '',
+      failureType: undefined,
+      signInErrorMessage: '',
+      googleAuthObject: null,
+      googleUser: null,
+      isSignedIn: false
     }
   },
   mounted() {
@@ -22,19 +37,58 @@ export default {
     } else {
       console.error('Service workers are not supported.');
     }
-    function updateSigninStatus(event) {
-      console.log(event)
-    }
-    var GoogleAuth;
-    gapi.load('client:auth2', initClient);
-    function initClient() {
-      gapi.client.init({
-        'apiKey': 'AIzaSyA4HBGkcCkWXXwOkKB0jdPqIaSCOTenR-k',
-        'clientId': '692884782115-u4o2n8dco40hjqa18b1agl9492m05l1j.apps.googleusercontent.com',
-        'scope': 'https://www.googleapis.com/auth/spreadsheets'
-      }).then(function () {
-        GoogleAuth = gapi.auth2.getAuthInstance();
-      });
+    this.initialize()
+    window.addEventListener('online',  this.updateConnectionStatus);
+    window.addEventListener('offline', this.updateConnectionStatus);
+  },
+  methods: {
+    initialize: function () {
+      if (navigator.onLine){
+        gapi.load('client:auth2', initClient.bind(this));
+        function initClient() {
+          gapi.client.init({
+            'apiKey': 'AIzaSyA4HBGkcCkWXXwOkKB0jdPqIaSCOTenR-k',
+            'clientId': '692884782115-u4o2n8dco40hjqa18b1agl9492m05l1j.apps.googleusercontent.com',
+            'scope': 'https://www.googleapis.com/auth/spreadsheets'
+          }).then(function () {
+            this.googleAuthObject = gapi.auth2.getAuthInstance();
+            this.isConnected = true;
+            this.applicationStatus = 'signIn';
+          }.bind(this));
+        }
+      }
+      else {
+        if (localStorage.getItem("questions") === null) {
+          this.applicationStatus = 'fatalError'
+          this.failureText = 'No local cache found, please connect your computer to the internet. Waiting for connection.'
+          this.failureType = 'init_network'
+        }
+      }
+    },
+    updateConnectionStatus: function(event) {
+      this.isConnected = navigator.onLine
+      if (this.applicationStatus === 'fatalError' && this.failureType === 'init_network') {
+        this.applicationStatus = 'initializing'
+        this.initialize()
+      }
+      if (this.isConnected === false && this.applicationStatus === 'signIn') {
+        this.initialize()
+      }
+    },
+    signIn: function() {
+      this.googleAuthObject.signIn().then(function(){
+        this.updateSignInStatus(true)
+      }.bind(this), function(){
+        this.updateSignInStatus(false)
+      }.bind(this))
+    },
+    updateSignInStatus: function(isSignedIn) {
+      this.isSignedIn = isSignedIn
+      if (isSignedIn && this.googleAuthObject.currentUser.get().hasGrantedScopes('https://www.googleapis.com/auth/spreadsheets')) {
+        this.googleUser = this.googleAuthObject.currentUser.get();
+        this.applicationStatus = 'scouting'
+        console.log(this.googleUser.getBasicProfile().getName())
+      }
     }
   }
 }
@@ -47,8 +101,19 @@ body, html {
   font-family: 'Arvo', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-size: 14px;
+  line-height: 1.4;
   margin: 0;
   box-sizing: border-box;
   background-color: #0a2634;
+}
+
+.mshadow {
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+}
+
+.mshadow:not(.static):hover {
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
 }
 </style>
---
+++ src/components/ScoutingForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div class="error">Unimplemented</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'scoutingform',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column nowrap;
+}
+.error {
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 3rem;
+  margin-bottom: 1rem;
+}
+.message {
+  color: #fff;
+  font-size: 1.5rem;
+  padding-left: 50px;
+  padding-right: 50px;
+  text-align: center;
+  max-width: 700px;
+}
+</style>
