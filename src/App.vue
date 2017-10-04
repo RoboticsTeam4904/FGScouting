30b8148ec147e38c94c31ec3748650ac2a94c18a
Izzy Swart <syntacticsugarglider@gmail.com>
2017-10-03 19:43:51 -0700

    Cleaned boilerplate, added service worker, started work on Sheets API connector

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+  <Loader v-if="applicationStatus==='initializing'"></Loader>
 </template>
 
 <script>
+import Loader from './components/Loader'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {Loader},
+  data() {
+    return {
+      applicationStatus: 'initializing'
+    }
+  },
+  mounted() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/static/worker.js').then(function(registration) {
+      }).catch(function(error) {
+        console.error('Service worker registration failed:', error);
+      });
+    } else {
+      console.error('Service workers are not supported.');
+    }
+    function updateSigninStatus(event) {
+      console.log(event)
+    }
+    var GoogleAuth;
+    gapi.load('client:auth2', initClient);
+    function initClient() {
+      gapi.client.init({
+        'apiKey': 'AIzaSyA4HBGkcCkWXXwOkKB0jdPqIaSCOTenR-k',
+        'clientId': '692884782115-u4o2n8dco40hjqa18b1agl9492m05l1j.apps.googleusercontent.com',
+        'scope': 'https://www.googleapis.com/auth/spreadsheets'
+      }).then(function () {
+        GoogleAuth = gapi.auth2.getAuthInstance();
+      });
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css?family=Arvo');
+
+body, html {
+  font-family: 'Arvo', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  box-sizing: border-box;
+  background-color: #0a2634;
 }
 </style>
